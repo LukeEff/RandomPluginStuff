2ebba9431c7a6ad41a3336f1367d50bb6be60d72
lukeeff <luke4nandez@gmail.com>
2020-05-25 22:02:08 -0700

    cleanup

--- src/main/java/io/github/lukeeff/spigottemplate/HUDLearning.java
+++ src/main/java/io/github/lukeeff/spigottemplate/HUDLearning.java
@@ -7,21 +7,10 @@ public class HUDLearning extends JavaPlugin {
 
     //@Getter private ProtocolManager protocolManager;
 
-    public void init() {
-        DolphinEntityTypeRegistry.init();
-    }
-
     @Override
     public void onEnable() {
-        init();
+        DolphinEntityTypeRegistry.init();
         getServer().getPluginManager().registerEvents(new Censor(this), this);
-        //Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Dolphin: " + DolphinEntityTypeRegistry.WRAPPED_DOLPHIN);
-        //Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "DolphinWrapper: " + DolphinTypesRegistry.WRAPPED_DOLPHIN_TYPE);
-
-        //this.protocolManager = ProtocolLibrary.getProtocolManager();
-        //IChunkAccess
-        //Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "Template successfully loaded!");
-
     }
 
     @Override
